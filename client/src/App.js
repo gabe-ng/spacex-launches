c320346c6067db9350123f6278daf355a7bc293e
Gabriel Ng <gabrielng15@gmail.com>
2018-12-02 17:16:58 -0800

    Set up react router, moment, classnames in UI (#6)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Launches from './components/Launches';
+import Launch from './components/launch';
 
 import './App.css';
 import Logo from './spacexlogo.png';
@@ -13,11 +15,14 @@ const client = new ApolloClient({
 class App extends Component {
   render() {
     return <ApolloProvider client={client}>
+      <Router>
         <div className="container">
           <img src={Logo} alt="SpaceX" style={{ width: 300, display: "block", margin: "auto" }} />
-          <Launches />
+          <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
         </div>
-      </ApolloProvider>;
+      </Router>
+    </ApolloProvider>;
   }
 }
 
--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import LaunchItem from './LaunchItem';
+import MissionKey from './MissionKey';
+
 
 const LAUNCHES_QUERY = gql`
     query LauchesQuery {
@@ -19,6 +21,7 @@ class Launches extends Component {
     return (
       <React.Fragment>
         <h1 className="display-4 my-3">Launches</h1>
+        <MissionKey />
         <Query query={LAUNCHES_QUERY}>
             {
                 ({ loading, error, data }) => {
